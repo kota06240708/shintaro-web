a5e58bc8ad5ce97eff80e8e7fe912f7284383631
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-06-26 15:38:04 +0900

    add: 画像を先読みを追加

--- src/shared/scripts/loading.js
+++ src/shared/scripts/loading.js
@@ -3,21 +3,27 @@ import { TweenLite } from 'gsap';
 
 const loading = () => {
   const loads = []; // 画像を読み込むpromiseを追加していく
+  const $$loading = document.getElementById('js-loading');
+  const $$main = document.getElementById('js-main');
 
   // 画像を先読み
   images.forEach(r => {
     const image = new Image();
     const load = new Promise(resolve => {
       image.src = r;
-      image.onload = () => resolve('成功');
+      image.onload = () => resolve();
     });
 
     loads.push(load);
   });
 
-  Promise.all(loads).then(values => {
-    console.log(values);
-    console.log(TweenLite);
+  Promise.all(loads).then(() => {
+    $$main.style.display = 'block';
+
+    TweenLite.to($$loading, 0.4, {
+      delay: 0.4,
+      autoAlpha: 0
+    });
   });
 };
 
