5ab20f194858b92987d4faa51cfe9fa86acc52de
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-06-26 12:42:03 +0900

    add: 画像のurlを追加

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+export const images = [
+  '/image/artist/ono.png',
+  '/image/artist/aduma.png',
+  '/image/artist/kota.png',
+  '/image/artist/hitomun.jpg',
+  '/image/artist/mieciaro.png',
+  '/image/work/mutek.png',
+  '/image/work/avant-garde.png',
+  '/image/work/sxsw.jpg',
+  '/image/work/waveequation.png',
+]
